f9c8e61a947884f33b89a86123a8ec0b7dba5bec
Bruno Barcarol Guimarães <bbgstb@gmail.com>
2013-08-31 14:30:16 -0300

    Adding script to create title headers.

---
+++ make_header.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Script to create title headers for the slides. To use it, just filter the text
+# through this script.
+# -d - shell design
+# -s - 80 columns
+# -a - center text horizontally
+boxes -d shell -s 80 -a hc
